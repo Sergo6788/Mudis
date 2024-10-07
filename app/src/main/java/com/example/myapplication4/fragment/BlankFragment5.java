566f45fbf38ce63ddd8389bc83b0f4b74e5626c1
Sergo6788 <fedyakov2006.sergey@gmail.com>
2024-10-07 10:29:20 +0300

    fragment move

--- app/src/main/java/com/example/myapplication4/fragment/BlankFragment5.java
+++ app/src/main/java/com/example/myapplication4/fragment/BlankFragment5.java
@@ -9,16 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication4.R;
+import com.example.myapplication4.databinding.FragmentBlankBinding;
 
 
 public class BlankFragment5 extends Fragment {
+    private FragmentBlankBinding binding;
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank5, container, false);
+
+        binding = FragmentBlankBinding.inflate(inflater);
+        return binding.getRoot();
     }
 }
